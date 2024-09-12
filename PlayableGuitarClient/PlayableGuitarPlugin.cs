b6c31d8aadab106fdedd700b3753223c5f30514b
Ryan <void.big@gmail.com>
2024-09-11 22:43:13 -0700

    Remove

--- PlayableGuitarClient/PlayableGuitarPlugin.cs
+++ PlayableGuitarClient/PlayableGuitarPlugin.cs
@@ -5,7 +5,7 @@ using PrivateRyan.PlayableGuitar.Patches;
 
 namespace PrivateRyan.PlayableGuitar
 {
-    [BepInPlugin("privateryan.playableguitar", "PlayableGuitar", "1.0.0")]
+    [BepInPlugin("privateryan.playableguitar", "PlayableGuitar", "1.1.0")]
     [BepInDependency("com.SPT.core", "3.9.0")]
     [BepInDependency("privateryan.tarkovmidi", "1.0.0")]
     public class PlayableGuitarPlugin : BaseUnityPlugin
